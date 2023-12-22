052a5d66e195d99a76785da5ea4702b0583d1a0e
NewGrafon <ultrafig73@gmail.com>
2023-12-22 21:18:45 +0300

    redis

--- src/cache/local-cache.module.ts
+++ src/cache/local-cache.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
 import { LocalCacheService } from './local-cache.service';
 import { CacheModule } from '@nestjs/cache-manager';
+import * as redisStore from 'cache-manager-redis-store';
 
 @Module({
   imports: [
     CacheModule.register({
       ttl: 60,
+      store: redisStore,
+      host: process.env.REDIS_HOST || 'localhost',
+      port: Number(process.env.REDIS_PORT) || 6379,
+      password: process.env.REDIS_PASSWORD,
     }),
   ],
   providers: [LocalCacheService],

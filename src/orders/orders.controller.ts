d20bf12b470ebb4e4107401c4d9a2ab3f9b8ed5f
NewGrafon <ultrafig73@gmail.com>
2024-03-14 21:34:13 +0300

    .

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -17,6 +17,7 @@ import { Role } from '../static/enums/users.enum';
 import { UserId } from '../static/decorators/user-id.decorator';
 import { CreateCartDto } from './dto/create-cart.dto';
 import { CartToyDto as CartToyDto } from './dto/cart-toy.dto';
+import { CloseOrdersDto } from './dto/close-order.dto';
 
 @Controller('orders')
 @UseGuards(AuthGuard)
@@ -35,14 +36,12 @@ export class OrdersController {
     return this.ordersService.removeFromCart(userId, cartToyDto);
   }
 
-  // need test
   @Post('confirm_cart')
   @RolesList(Role.Worker)
   confirmCart(@UserId() userId: number, @Body() createCartDto: CreateCartDto) {
     return this.ordersService.confirmCart(userId, createCartDto);
   }
 
-  // need test
   @Patch('take/:cartTimestamp')
   @RolesList(Role.Deliver)
   takeOrders(
@@ -52,32 +51,31 @@ export class OrdersController {
     return this.ordersService.takeOrders(userId, cartTimestamp);
   }
 
-  // need test
   @Patch('close/:cartTimestamp/:isFinishedNotCancel')
   @RolesList(Role.Worker, Role.Deliver)
   closeOrders(
     @Param('cartTimestamp') cartTimestamp: string,
     @Param('isFinishedNotCancel', ParseBoolPipe) isFinishedNotCancel: boolean,
     @UserId() userId: number,
+    @Body() closeOrdersDto: CloseOrdersDto,
   ) {
     return this.ordersService.closeOrders(
       cartTimestamp,
       isFinishedNotCancel,
       userId,
+      closeOrdersDto,
     );
   }
 
-  // need test
   @Delete('cancel/:cartTimestamp')
   @RolesList(Role.Worker)
   cancelOrders(
     @Param('cartTimestamp') cartTimestamp: string,
     @UserId() userId: number,
   ) {
-    return this.ordersService.cancelOrder(cartTimestamp, userId);
+    return this.ordersService.cancelOrders(cartTimestamp, userId);
   }
 
-  // need test
   @Get('get_all')
   @RolesList()
   findAll() {
@@ -90,7 +88,12 @@ export class OrdersController {
     return this.ordersService.getColorsInfo();
   }
 
-  // need test
+  @Get('get_by_timestamp/:cartTimestamp')
+  @RolesList()
+  findOneByCartTimestamp(@Param('cartTimestamp') cartTimestamp: string) {
+    return this.ordersService.findByCartTimestamp(cartTimestamp);
+  }
+
   @Get(':id')
   @RolesList()
   findOne(@Param('id', ParseIntPipe) id: number) {
